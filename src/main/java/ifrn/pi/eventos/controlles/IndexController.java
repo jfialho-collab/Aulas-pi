5e65a3e2fd72872f03e3f24c44089ededf2efd45
JULIANA MARTINS FIALHO <20231204010006@ifrn.local>
2026-08-18 15:27:52 -0300

    Colocando os arquivos iniciais

---
+++ src/main/java/ifrn/pi/eventos/controlles/IndexController.java
@@ -0,0 +1,16 @@
+package ifrn.pi.eventos.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("chamou o método index");
+		return "home";
+	}
+	
+}
